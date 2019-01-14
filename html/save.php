a6118198ed8bde47e9462d6fd442332424db3801
Adam Clemons <nohbodyz@gmail.com>
2019-01-14 00:39:03 -0700

    Change file transfer for offline results from loading in memory to chunked transfer.

--- html/save.php
+++ html/save.php
@@ -8,13 +8,15 @@
     if(file_exists($filePath)) {
         header('Content-Description: File Transfer');
         header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'. $filePath .'"');
+        header('Content-Disposition: attachment; filename="'. $fileName .'"');
         header('Expires: 0');
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
         header('Content-Length: ' . filesize($filePath));
         flush(); // Flush system output buffer
-        readfile($filePath);
+
+        $fp = fopen($filePath, 'rb');
+        fpassthru($fp);
         exit;
     }
 ?>
